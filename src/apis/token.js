2e697acdd18a07f8afc903adafc7ec1f91fd658c
zhangziliang <15270216967@163.com>
2021-03-29 18:33:48 +0800

    @ADDR

--- src/apis/token.js
+++ src/apis/token.js
@@ -41,17 +41,35 @@ export const  contractType = `ttex`      //18个0
 
 
 
-// huiwanToken 合约地址   公共
-export const huiwanTokenAddr = "0xD05D1Bc082c9AbB39C57907a73Ce1DFCC30c70A4";
+// // huiwanToken 合约地址   公共
+// export const huiwanTokenAddr = "0xD05D1Bc082c9AbB39C57907a73Ce1DFCC30c70A4";
 
-// usdtToken 合约地址   公共 
-export const usdtTokenAddr = "0xa71edc38d189767582c38a3145b5873052c3e47a";
+// // usdtToken 合约地址   公共 
+// export const usdtTokenAddr = "0xa71edc38d189767582c38a3145b5873052c3e47a";
 
-// huiwanUsdtLoop   第一个池子 合约地址
-export  const huiwanUsdtLoopAddr = "0x49aE7072fB511DBe80B5144D50504fA08c90f80d";
+// // huiwanUsdtLoop   第一个池子 合约地址
+// export  const huiwanUsdtLoopAddr = "0x49aE7072fB511DBe80B5144D50504fA08c90f80d";
 
-// mdex huiwan-usdt      第一个池子  配对合约地址
-export const huiwanUsdtMdexAddr = "0x9013ee42412158dac0f2ff6fcad9564484851748";
+// // mdex huiwan-usdt      第一个池子  配对合约地址
+// export const huiwanUsdtMdexAddr = "0x9013ee42412158dac0f2ff6fcad9564484851748";
 
-// 单币池 第二个池子
-export const huiwanSinglePoolAddr = '0x8920b69Bce345bd8731B8B229baA7dCD7eABeac8'
+// // 单币池 第二个池子
+// export const huiwanSinglePoolAddr = '0x8920b69Bce345bd8731B8B229baA7dCD7eABeac8'
+
+
+
+
+// huiwanToken 合约地址   公共
+export const huiwanTokenAddr = "0x3eF3A1F98ED9afC9EeE8C23Ed8F46C2708eF427C";
+
+// usdtToken 合约地址   公共 
+export const usdtTokenAddr = "0xa71edc38d189767582c38a3145b5873052c3e47a";
+
+// huiwanUsdtLoop   第一个池子 合约地址
+export  const huiwanUsdtLoopAddr = "0xe7DC285415DbEF72ac078364ba1f372e2B281b07";
+
+// mdex huiwan-usdt      第一个池子  配对合约地址
+export const huiwanUsdtMdexAddr = "0x2a859e506e44427798d8d7770a0107281346ee66";
+
+// 单币池 第二个池子
+export const huiwanSinglePoolAddr = '0x56E8a72bfCEF4c7d2eA2dF0a60bd2fad48Bb298b'
