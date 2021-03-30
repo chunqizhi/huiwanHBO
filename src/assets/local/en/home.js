354262fc92b4c225b0f3634164296c0d350c229b
zhangziliang <15270216967@163.com>
2021-03-30 12:14:27 +0800

    #语言

--- src/assets/local/en/home.js
+++ src/assets/local/en/home.js
@@ -4,7 +4,7 @@ export default  {
     title01: '1. Break down the search engine barrier',
     title02: '2. Improve efficiency',
     title03: '3. Refuse monopolies',
-    title04: '3. Refuse monopolies',
+    title04: '4. Nodal incentive',
     desc01: 'Blockchain Search Artifact will provide users with cross-chain resource search services by building indexers on content and resource based public chains, while also solving the search method for each public chain',
     desc02: 'AI technology labels resources and provides users with more convenient personalized recommendations by using the tagging system',
     desc03: 'Centralized search engine is caused by multiple interests, gradually losing the bottom line and the real meaning of search engine',
