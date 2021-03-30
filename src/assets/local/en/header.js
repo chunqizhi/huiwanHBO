d95ea94c2b94bc1501eff25a62351967d97ef682
zhangziliang <15270216967@163.com>
2021-03-30 15:24:37 +0800

    #文字

--- src/assets/local/en/header.js
+++ src/assets/local/en/header.js
@@ -4,7 +4,7 @@ export default  {
     nav02:'Pool',
     nav03:'Application',
     nav04:'Audit',
-    nav05:'Mune',
+    nav05:'Menu',
     desc01:'BlockChain Search Artifacts is an innovative intelligent search engine.',
     desc02:'BlockChain Search Artifacts is an innovative intelligent search engine. Deposit BSA to earn more tokens'
 }
