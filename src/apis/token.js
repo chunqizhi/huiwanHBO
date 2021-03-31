f8d2bf3270c573e512302e81bbdb51fac4640c93
chunqizhi <1558763837@qq.com>
2021-03-31 11:38:58 +0800

    Add bsa/ht pool

--- src/apis/token.js
+++ src/apis/token.js
@@ -75,25 +75,25 @@ export const contractType = `ttex` //18个0
 
 
 // huiwanToken 合约地址   公共
-export const huiwanTokenAddr = "0x8A37079c49e7aE084eb36a220756C86e101e7047";
+export const huiwanTokenAddr = "0x3eF3A1F98ED9afC9EeE8C23Ed8F46C2708eF427C";
 
 // usdtToken 合约地址   公共 
 export const usdtTokenAddr = "0xa71edc38d189767582c38a3145b5873052c3e47a";
 
 // huiwanUsdtLoop   第一个池子 合约地址
-export const huiwanUsdtLoopAddr = "0x3F8F58FD96098966A6053AB145864303210e25B7";
+export const huiwanUsdtLoopAddr = "0xb5509791eA8730F2628bb84427e5E2826FCc45A8";
 
 // mdex huiwan-usdt      第一个池子  配对合约地址
-export const huiwanUsdtMdexAddr = "0x29d6a60692faaba233e947686acf7dc5dd9c653d";
+export const huiwanUsdtMdexAddr = "0x949102338A4810c6BD6B75a1d633F803bfAC5055";
 
 // 单币池 第二个池子
-export const huiwanSinglePoolAddr = '0xee1D16F6Fb9885bDAFdC3d17570bf13E881b2c32'
+export const huiwanSinglePoolAddr = '0x56E8a72bfCEF4c7d2eA2dF0a60bd2fad48Bb298b'
 
 
 // HT addr
 export const huiwanHTAddr = "0x5545153ccfca01fbd7dd11c0b23ba694d9509a6f"
 
 // HT Mdex Addr
-export const huiwanHTMdexAddr = "0x6e213bc14a5d3678cf192e94992a181454001c26"
+export const huiwanHTMdexAddr = "0xfefd13f95fa8746788cc2d23bcf03f2fa9690676"
     //  HT pool 地址
-export const huiwanHTPoolAddr = "0x47Eed3BFBB685A48A2841a3c4Ed8f839d3638910"
+export const huiwanHTPoolAddr = "0x2AcF66B4D08f9b8129FF06f73f28374A0842eb97"
